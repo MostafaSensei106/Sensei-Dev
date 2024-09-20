a35680ca8d6d0773afd4598aade4cd065d34505e
MostafaSensei106 <138288138+MostafaSensei106@users.noreply.github.com>
2024-09-20 06:17:49 +0300

    Home Section

--- app/com/home/sensei-home.tsx
+++ app/com/home/sensei-home.tsx
@@ -1,42 +1,36 @@
-import Image from 'next/image';
-import { LinkedIn, GitHub, Telegram } from '@mui/icons-material'; // استيراد أيقونات MUI
-import { Person, Description } from '@mui/icons-material'; // استيراد أيقونات إضافية
 import styles from './sensei-home.module.css';
 
 function SenseiHome() {
     return (
         <section className={styles.home} id="Home">
             <div className={styles.home_img}>
-                <Image
+                <img
                     src="/Assets/Images/Sensei.jpg"
                     alt="Home_Image"
-                    layout="responsive"
                     className={styles.image}
-                    width={3600}
-                    height={3600}
                 />
             </div>
             <div className={styles.homeContent}>
-                <h1>Hi, It's <span className={styles.highlight}>Mostafa Sensei</span></h1>
-                <h3 className={styles.typingText}>I'm a <span className={styles.typingHighlight}></span></h3>
+                <h1>
+                Hi, It's <span className={styles.highlight}>Mostafa Sensei</span>
+                </h1>
+                <h3 className={styles.typingText}>
+                    I'm a <span className={styles.typingHighlight}></span>
+                </h3>
                 <p>
-                    I am currently a college student with a deep passion for software development and digital art.
-                    Specializing in Python and Flutter, I develop applications with a focus on stability, security, and user
-                    satisfaction. My expertise spans Python-based computer vision projects, where I leverage advanced techniques to solve complex
-                    problems, and a strong interest in Android systems, which drives my quest for creating seamless mobile
-                    experiences. In my role as an independent Flutter developer.
+                    I'm a college student specializing in Python and Flutter, focusing on stable and secure app development. I have experience in Python-based computer vision projects and improving mobile experiences through Flutter development.
                 </p>
                 <div className={styles.socialIcon}>
-                    <a href="#"><LinkedIn /></a>
-                    <a href="#"><GitHub /></a>
-                    <a href="#"><Telegram /></a>
+                    <a href="https://www.linkedin.com/in/mostafa-mahmoud-963a78235/" target={"_blank"}> <i className="fa-brands fa-linkedin"></i></a>
+                    <a href="https://github.com/MostafaSensei106" target={"_blank"}><i className="fa-brands fa-github"></i></a>
+                    <a href="https://t.me/Mostafa_Sensie106" target={"_blank"}> <i className="fa-brands fa-telegram"></i></a>
                 </div>
                 <div className={styles.homeButton}>
                     <a href="#" className={`${styles.btn} ${styles.btn1}`}>
-                        Hire Me <Person style={{fontSize: '1.2rem', marginLeft: '0.5rem'}} />
+                        Hire Me <i className="fa-solid fa-user-secret"></i>
                     </a>
                     <a href="#" className={`${styles.btn} ${styles.btn2}`}>
-                        Download CV <Description style={{fontSize: '1.2rem', marginLeft: '0.5rem'}} />
+                        Download CV <i className="fa-solid fa-file-pdf"></i>
                     </a>
                 </div>
             </div>
