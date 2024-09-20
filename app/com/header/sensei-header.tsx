5f2e3a6715d4a9573bd5ec719c2d9047f45fea11
MostafaSensei106 <138288138+MostafaSensei106@users.noreply.github.com>
2024-09-20 03:58:34 +0300

    Header Css

--- app/com/header/sensei-header.tsx
+++ app/com/header/sensei-header.tsx
@@ -1,17 +1,18 @@
-
-import './sensei-header.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './sensei-header.css';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 function SenseiHeader() {
     return (
         <header className="header">
             <a href="#" className="logo">Mostafa Sensei</a>
-            <i className="fa-solid fa-bars" id="MenuIcon"></i>
+            <FontAwesomeIcon className="MenuIcon" icon={faBars} size="lg" />
             <nav className="navbar">
                 <a href="#home" className="active">Home</a>
-                <a href="#Service">Service</a>
-                <a href="#Education">Education</a>
-                <a href="#Projects">Projects</a>
-                <a href="#Contact Me">Contact Me</a>
+                <a href="#service">Service</a>
+                <a href="#education">Education</a>
+                <a href="#projects">Projects</a>
+                <a href="#contact-me">Contact Me</a>
             </nav>
         </header>
     );
