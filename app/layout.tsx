fb61facf33c0a2ee09ee7a9ad078e01f4bdd68e8
MostafaSensei106 <138288138+MostafaSensei106@users.noreply.github.com>
2024-09-27 04:52:17 +0300

    Mostafa Sensei Site

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,9 +30,6 @@ export default function RootLayout({
     <html lang="en">
     <head>
       <title>Mostafa Sensei</title>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
-            integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
-            crossOrigin="anonymous" referrerPolicy="no-referrer"/>
     </head>
     <body /*className={`${geistSans.variable} ${geistMono.variable}`}*/>
     {children}
