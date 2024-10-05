2a286f713b457db5165768d609fdfe6aedaa453d
MostafaSensei106 <138288138+MostafaSensei106@users.noreply.github.com>
2024-10-05 20:33:12 +0300

    Mostafa Sensei Site

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 import nextPWA from 'next-pwa';
 
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+};
 
 const withPWA = nextPWA({
     dest: 'public',
     disable: process.env.NODE_ENV === 'development'
 });
 
-export default withPWA(nextConfig);
+try {
+    export default withPWA(nextConfig);
+} catch (error) {
+    console.error('Error in Next.js config:', error);
+    export default nextConfig;
+}
