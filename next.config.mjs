28ad969859575a2e51e7270a9910b0d56c17a4b9
MostafaSensei106 <138288138+MostafaSensei106@users.noreply.github.com>
2024-10-10 18:40:58 +0300

    Mostafa Sensei Site

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,6 @@
-import withPWA from 'next-pwa';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    output: 'export',
-    images: {
-        unoptimized: true,
-    },
-};
+const nextConfig = {};
 
-const pwaConfig = {
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-    register: true,
-    skipWaiting: true,
-};
+export default nextConfig;
 
-export default withPWA(pwaConfig)(nextConfig);
