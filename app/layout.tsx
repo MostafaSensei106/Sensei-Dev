16d12da1b637871a80b38ddffd58ea2125cbef55
MostafaSensei106 <138288138+MostafaSensei106@users.noreply.github.com>
2024-10-14 11:04:01 +0300

    Release Website ================

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,45 @@
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import "./globals.css";
 import React from "react";
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import { config } from '@fortawesome/fontawesome-svg-core';
-
+import {config} from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss = false;
 
+/**
+ * @author Mostafa Sensei106
+ * @description A layout component that includes the header, footer, and other components.
+ * @returns A layout component that includes the header, footer, and other components.
+ */
 
-export const metadata: Metadata = {
-    description: "Hi My Name is Mostafa",
+// Custom Metadata
+type CustomMetadata = Metadata & {
+    description: string; // The description of the page
+    keywords: string;// The keywords of the page
+    author: string;// The author of the page
+    viewport: string;// The viewport of the page
+    charset: string;// The character set of the page
+    themeColor?: string;// The theme color of the page
+    ogTitle?: string;// The title of the page for social media
+    ogDescription?: string;// The description of the page for social media
+    ogUrl?: string;// The URL of the page for social media
 };
+
+// Metadata
+// The metadata is used to set the title, description, keywords, author, viewport, and charset of the page.
+// The metadata is also used to set the theme color of the page.
+// The metadata is also used to set the title of the page for social media.
+export const metadata: CustomMetadata = {
+    description: "Hello! I'm Mostafa Mahmoud, a Software Engineer specializing in Flutter, backend development, and AI. Explore my projects, services ArtGallery, and learn more about my skills.",
+    keywords: "Mostafa Mahmoud, Mostafa Sensei, Flutter,Flutter Developer, Backend Development, AI, Sensei Dev, TypeScript,Dart,Python, Programming, UI Design, Artificial Intelligence, App Development, Mobile App Development, Game Development, Art, Digital Art, Web Design, Web Development",
+    author: "Mostafa Sensei106",// Sets the author of the page
+    viewport: "width=device-width, initial-scale=1.0",// Sets the viewport of the page
+    charset: "UTF-8",// Sets the character encoding of the document
+    themeColor: "#000",  // Sets the browser theme color
+    ogTitle: "Mostafa Mahmoud - Web Developer & Designer",  // Title for social media
+    ogDescription: "Discover the projects and services of Mostafa Mahmoud, specializing in Flutter, Backend development, AI, Digital Art",  // Description for social media
+    ogUrl: "https://mostafasensei106.github.io/Sensei-Dev",  // URL of your website
+};
+
 
 export default function RootLayout({
                                        children,
@@ -44,12 +74,12 @@ export default function RootLayout({
                 rel="stylesheet"/>
         </head>
         <body>
-            {/*// The children prop is the content which is passed to this*/}
-            {/*// component by its parent. It is rendered here.*/}
-            {children}
-            {/*// The ServiceWorkerRegistration component is used to*/}
-            {/*// register the service worker which is used to implement*/}
-            {/*// offline support for the application.*/}
+        {/*// The children prop is the content which is passed to this*/}
+        {/*// component by its parent. It is rendered here.*/}
+        {children}
+        {/*// The ServiceWorkerRegistration component is used to*/}
+        {/*// register the service worker which is used to implement*/}
+        {/*// offline support for the application.*/}
         </body>
         </html>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -5,23 +5,27 @@ import SenseiEducation from "@/app/com/education/sensei-education";
 import SenseiProjects from "@/app/com/services/sensei-projects";
 import SenseiArt from "./com/art_gallery/sensei-art";
 import SenseiContact from "@/app/com/contact_me/sensei-contact";
-import Sensei_loader from "@/app/com/loader/sensei_loader";
+import Senseiloader from "@/app/com/loader/sensei_loader";
 import AnimatedBackground from "@/app/com/animated_background/animated_background";
 
-function Home() {
+// Main component
+// @returns {JSX.Element}
+// @author Mostafa Sensei106
+// @description A functional component that renders the page of the website.
+function Main() {
     return (
         <>
-            <Sensei_loader />
-            <SenseiHeader />
-            <AnimatedBackground/>
-            <SenseiHome />
-            <SenseiServicesProjects />
-            <SenseiEducation />
-            <SenseiProjects />
-            <SenseiArt />
-            <SenseiContact />
+            <Senseiloader />// This component displays a loading spinner until the page is fully loaded
+            <SenseiHeader />// The main header component
+            <AnimatedBackground/>// The animated background
+            <SenseiHome />// The home component
+            <SenseiServicesProjects />// The  services component
+            <SenseiEducation />// The education component
+            <SenseiProjects />// The projects component
+            <SenseiArt />// The art gallery component
+            <SenseiContact />// The contact me component
         </>
     );
 }
 
-export  default Home;
+export  default Main;
