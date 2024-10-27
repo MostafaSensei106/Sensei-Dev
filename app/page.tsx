ade4a9578aa9682df4a9c5a582a2a085bfc1c94e
MostafaSensei106 <138288138+MostafaSensei106@users.noreply.github.com>
2024-10-27 23:28:47 +0300

    Art

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import SenseiHeader from "@/app/com/header/sensei-header";
 import SenseiHome from "@/app/com/home/sensei-home";
 import SenseiServicesProjects from "./com/services/sensei-services-projects";
 import SenseiEducation from "@/app/com/education/sensei-education";
-import SenseiProjects from "@/app/com/services/sensei-projects";
+// import SenseiProjects from "@/app/com/services/sensei-projects";
 import SenseiArt from "./com/art_gallery/sensei-art";
 import SenseiContact from "@/app/com/contact_me/sensei-contact";
 import Senseiloader from "@/app/com/loader/sensei_loader";
