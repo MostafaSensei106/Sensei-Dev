65a00da94396535f3d34924d01a8bc6d9f7d734f
Eng-Ahmed-Emad <ahmedemadeldeen77@gmail.com>
2025-10-01 09:34:46 +0300

    Added Whatsapp links And Sara7a :)

--- app/com/contact_me/sensei-contact.tsx
+++ app/com/contact_me/sensei-contact.tsx
@@ -107,6 +107,16 @@ const SenseiContact = (): JSX.Element => {
                                         {/* GitHub icon */}
                                         <i className="fa-brands fa-github"></i>
                                     </a>
+                                    <a
+                                        aria-label="Go to Saraha"
+                                        href="https://17833478947642.sarhne.com"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        title="Saraha"
+                                    >
+                                        <FontAwesomeIcon icon={faUserSecret} />
+                                    </a>
+
 
 
                                 </div>
